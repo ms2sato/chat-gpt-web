d25833ac5b683eb776576ab9af13d69b270e601f
Masashi Sato <ms2.sato@gmail.com>
2023-03-26 14:38:17 +0900

    the first

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, "0.0.0.0", () => console.log(`app http://localhost:${port}!`));
